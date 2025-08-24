1383c052e59f48c6da8b226c825cf234f334062c
Juan Miñaca <jminaca@espol.edu.ec>
2025-08-23 23:12:21 -0500

    Update Boleto.java
    Refactorin seccion B

--- src/Boleto.java
+++ src/Boleto.java
@@ -1,18 +1,27 @@
 import java.util.Date;
 
 public class Boleto {
-    public enum Estado { DISPONIBLE, RESERVADO, VENDIDO, CANCELADO }
+    public enum Estado { DISPONIBLE, RESERVADO, VENDIDO, CANCELADO, EXPIRADO } 
 
     private int id;
     private Evento evento;
     private Estado estado;
     private Date fechaCompra;
+    private double precio; 
 
     public Boleto(int id, Evento evento) {
+        this(id, evento, 0.0);
+    }
+
+    public Boleto(int id, Evento evento, double precio) {
+        if (precio < 0) throw new IllegalArgumentException("El precio no puede ser negativo");
         this.id = id;
         this.evento = evento;
+        this.precio = precio;
         this.estado = Estado.DISPONIBLE;
     }
+
+    public double getPrecio() { return precio; }
 
     // Getters y setters
     public int getId() { return id; }
